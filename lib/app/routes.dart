3b6cf0f1a25198ed8dfde066a95666222cb8ec8b
wrteam-priyansh <wrteam.priyansh@gmail.com>
2022-07-12 17:02:15 +0530

    removed unused files

--- lib/app/routes.dart
+++ lib/app/routes.dart
@@ -16,7 +16,6 @@ import 'package:eschool_teacher/ui/screens/splashScreen.dart';
 import 'package:eschool_teacher/ui/screens/studentDetails/studentDetailsScreen.dart';
 import 'package:eschool_teacher/ui/screens/studyMaterials/studyMaterialsScreen.dart';
 import 'package:eschool_teacher/ui/screens/subject/subjectScreen.dart';
-import 'package:eschool_teacher/ui/screens/uploadFiles/uploadFilesScreen.dart';
 import 'package:eschool_teacher/ui/screens/uploadVideos/uploadVideosScreen.dart';
 import 'package:eschool_teacher/ui/screens/takeAttendance/takeAttendanceScreen.dart';
 import 'package:flutter/cupertino.dart';
@@ -42,8 +41,6 @@ class Routes {
   static const String addAssignment = "/addAssignment";
 
   static const String uploadVideos = "/uploadVideos";
-
-  static const String uploadFiles = "/uploadFiles";
 
   static const String takeAttendance = "/takeAttendance";
 
@@ -107,10 +104,7 @@ class Routes {
         {
           return CupertinoPageRoute(builder: (_) => UploadVideosScreen());
         }
-      case uploadFiles:
-        {
-          return CupertinoPageRoute(builder: (_) => UploadFilesScreen());
-        }
+
       case takeAttendance:
         {
           return CupertinoPageRoute(builder: (_) => TakeAttendanceScreen());
