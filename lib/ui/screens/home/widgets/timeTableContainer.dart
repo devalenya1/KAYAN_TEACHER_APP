e91e891be92b51537276177324060f58184396df
wrteam-priyansh <wrteam.priyansh@gmail.com>
2022-08-05 15:48:02 +0530

    completed v-1.0.0

--- lib/ui/screens/home/widgets/timeTableContainer.dart
+++ lib/ui/screens/home/widgets/timeTableContainer.dart
@@ -1,4 +1,3 @@
-import 'package:cached_network_image/cached_network_image.dart';
 import 'package:eschool_teacher/cubits/timeTableCubit.dart';
 import 'package:eschool_teacher/data/models/timeTableSlot.dart';
 import 'package:eschool_teacher/ui/screens/class/widgets/subjectImageContainer.dart';
