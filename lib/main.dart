1dede672d4582f9650f8896dd567d907749a15db
wrteam-priyansh <wrteam.priyansh@gmail.com>
2022-08-27 09:43:51 +0530

    update to v-1.0.0

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:eschool_teacher/app/app.dart';
 
-//
+//v-1.0.0
 void main() {
   initializeApp();
 }
--- lib/utils/constants.dart
+++ lib/utils/constants.dart
@@ -1,7 +1,6 @@
 //database urls
 //Please add your admin panel url here and make sure you do not add '/' at the end of the url
-const String baseUrl =
-    "https://e-school.wrteam.in"; //"https://testschool.wrteam.in";
+const String baseUrl = "https://e-school.wrteam.in";
 
 const String databaseUrl = "$baseUrl/api/";
 
